b7fda0560b8480f9fb320c5f467fe79884b04555
Bruno <bgarciamoura@gmail.com>
2020-09-11 16:06:46 -0300

    Updated the style to match with flexbox css

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,18 +5,21 @@ import './style.css';
 const App = () => {
     return (
         <div className="container">
-            <div className="wrapper">
-                <div className="box">
-                    <p>
-                        border-radius: px px px px;
-                        <br />
-                        -moz-border-radius: px px px px;
-                        <br />
-                        -webkit-border-radius: px px px px;
-                    </p>
-                </div>
+            <div className="topinputs">
                 <input type="text" name="topleft" id="topleft" />
                 <input type="text" name="topright" id="topright" />
+            </div>
+
+            <div className="box">
+                <p>
+                    border-radius: 10px 10px 10px 10px;
+                    <br />
+                    -moz-border-radius: 10px 10px 10px 10px;
+                    <br />
+                    -webkit-border-radius: 10px 10px 10px 10px;
+                </p>
+            </div>
+            <div className="bottominputs">
                 <input type="text" name="bottomleft" id="bottomleft" />
                 <input type="text" name="bottomright" id="bottomright" />
             </div>
