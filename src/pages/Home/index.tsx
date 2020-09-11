3e5dd14bc062d236debcc85977aceed66372024c
Bruno <bgarciamoura@gmail.com>
2020-09-11 14:08:22 -0300

    Created the inputs and the main box

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import './style.css';
+
 const App = () => {
     return (
         <div className="container">
-            <h1>Oi, Bruno</h1>
+            <div className="wrapper">
+                <div className="box">
+                    <p>
+                        border-radius: px px px px;
+                        <br />
+                        -moz-border-radius: px px px px;
+                        <br />
+                        -webkit-border-radius: px px px px;
+                    </p>
+                </div>
+                <input type="text" name="topleft" id="topleft" />
+                <input type="text" name="topright" id="topright" />
+                <input type="text" name="bottomleft" id="bottomleft" />
+                <input type="text" name="bottomright" id="bottomright" />
+            </div>
         </div>
     );
 };
